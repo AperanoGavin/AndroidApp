a48affe5bdd5ca5e5d1a8cd5d4e0eab60a2a78be
Gaveen Aperano Mouloungui <gaveen@MacBook-Pro-de-Gaveen.local>
2022-04-29 16:23:16 +0200

    first app android

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btn1=findViewById(R.id.btn_un);//R est une classe générer automatiquement qui recupère tout ce qui est dans le file res
+        this.btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
 }
