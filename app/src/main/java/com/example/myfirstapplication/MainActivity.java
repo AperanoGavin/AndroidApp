52aaede71c37eae2f5436e1ebeb58b48c2db8297
Gaveen Aperano Mouloungui <gaveen@pc-4-143.customer.ask4.lan>
2022-06-08 23:19:16 +0200

    some mod

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private Button btn1;
     private Button btn2;
     private Button btn3;
+    private ImageView imgg;
     private EditText edt;
+
+
+
 
 
 
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         this.btn1=findViewById(R.id.btn_un);//R est une classe générer automatiquement qui recupère tout ce qui est dans le file res
         this.btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +68,17 @@ public class MainActivity extends AppCompatActivity {
                         }
                     })
                     .show();
+
+            }
+        });
+
+        this.imgg=(ImageView) findViewById(R.id.imgg);
+        imgg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent en = new Intent(MainActivity.this, ImageActivity.class);
+                startActivity(en);
 
             }
         });
